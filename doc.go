9643a1978b23f962bb76bfe304079b276f4faff5
George Haidar <ghaidar0@gmail.com>
2015-09-21 18:06:59 +1000

    added package documentation

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Package ohauth allows the creation of OAuth 2.0 providers.
+//
+// The package defines several interfaces that must be implemented and used to
+// configure a Provider. The provider can then be incorporated into a web
+// service that provides OAuth 2.0 authorisation functionality.
+//
+// As a starting point, it is best to look at the Provider documentation and
+// proceeding from there to discover the rest of this library.
+package ohauth
